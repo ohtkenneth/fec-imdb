f15a69cd3719cc7e6b22e6305c552eb1a58838f4
Kenneth Oh <ohtkenneth@gmail.com>
2018-10-22 19:53:17 -0700

    Clean up file and tests

--- src/client/Components/tests/App.unit.test.js
+++ src/client/Components/tests/App.unit.test.js
@@ -15,18 +15,6 @@ import FAQ from '../FAQ.jsx';
 import Footer from '../Footer.jsx';
 
 describe('<App />', () => {
-  it ('renders all seven child components', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find(PhotoList)).to.be.true;
-    expect(wrapper.find(CastList)).to.be.true;
-    expect(wrapper.find(Storyline)).to.be.true;
-    expect(wrapper.find(Details)).to.be.true;
-    expect(wrapper.find(BoxOffice)).to.be.true;
-    expect(wrapper.find(DidYouKnow)).to.be.true;
-    expect(wrapper.find(FAQ)).to.be.true;
-    expect(wrapper.find(Footer)).to.be.true;
-  });
-  
   it('calls componentDidMount', () => {
     sinon.spy(App.prototype, 'componentDidMount');
     const wrapper = mount(<App />);
